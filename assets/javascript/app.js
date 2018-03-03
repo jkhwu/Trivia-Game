81f97c5b0bfbeddf7724e1d0786292d0bd00c4eb
Janet Hwu <Janet@MacBook-Pro-2.local>
2018-03-02 19:26:32 -0800

    added gifs, completed timer function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 // VARIABLES
 const questionBank = [{
         q: 'What was the last Broadway musical Rodgers and Hammerstein created, which starred Mary Martin and debuted in 1959?',
-        a: 2,
+        aIndex: 2,
         choices: ['"Allegro"', '"South Pacific"', '"The Sound of Music"', '"The King and I"'],
         image: "assets/images/soundofmusic.gif"
     },
@@ -9,125 +9,137 @@ const questionBank = [{
         q: 'The TKTS booth, which has been around since 1973, offers what?',
         aIndex: 0,
         choices: ['Discount theater tickets', 'Free movie passes', 'Specialty ice cream', 'Horse and buggy rides'],
-        image: "assets/images/"
+        image: "assets/images/tkts.gif"
     },
     {
         q: 'A helicopter was the unforgettable set piece of this musical, which opened in 1991:',
         aIndex: 2,
         choices: ["Evita", "This is the Army", "Miss Saigon", "Big Deal"],
-        image: "assets/images/"
+        image: "assets/images/misssaigon.gif"
     },
     {
         q: 'Which of these original cast members from "Wicked" made their Broadway debut in the musical "Rent?"',
         aIndex: 1,
         choices: ['Kristin Chenoweth', 'Idina Menzel', 'Joel Grey', 'Nobert Leo Butz'],
-        image: "assets/images/"
+        image: "assets/images/idina.gif"
     },
     {
         q: 'The first song in the original production of "Cabaret" opens with which of these lines?',
         aIndex: 3,
         choices: ['"Come to the cabaret"', '"Hey, big spender"', '"Come on, babe"', '"Wilkommen, bienvenue"'],
-        image: "assets/images/"
+        image: "assets/images/cabaret.gif"
     },
     {
         q: 'Which titular Broadway character is the "demon barber"?',
         aIndex: 0,
         choices: ['Sweeney Todd', 'Oliver', 'Annie', 'Peter Pan'],
-        image: "assets/images/"
+        image: "assets/images/sweeneytodd.gif"
     },
     {
         q: 'Which Broadway production once starred Ricky Martin?',
         aIndex: 3,
         choices: ['"West Side Story"', '"Cats"', '"The Phantom of the Opera"', '"Les Miserables"'],
-        image: "assets/images/"
+        image: "assets/images/ricky.gif"
     },
     {
         q: 'What year did "The Phantom of the Opera" first entertain musical fans?',
         aIndex: 1,
         choices: ['1976', '1986', '1989', '1992'],
-        image: "assets/images/"
+        image: "assets/images/phantom.gif"
     },
     {
         q: 'What musical features the song "Memory"?',
         aIndex: 3,
         choices: ['Chicago', 'The King and I', 'Hairspray', 'Cats'],
-        image: "assets/images/"
+        image: "assets/images/cats.gif"
     },
     {
         q: 'Which "Les Mis" character serves as the icon for the show?',
         aIndex: 0,
         choices: ['Cosette', 'Fantine', 'Eponine', 'Jean Valjean'],
-        image: "assets/images/"
+        image: "assets/images/lesmis.gif"
     },
     {
         q: 'What group\47s music is featured in "Mamma Mia!"?',
         aIndex: 2,
         choices: ['The Partridge Family', 'The Monkees', 'ABBA', 'The Beatles'],
-        image: "assets/images/"
+        image: "assets/images/abba.gif"
     },
     {
         q: 'What musical is based on a book by Gregory Maguire?',
         aIndex: 0,
         choices: ['"Wicked"', '"The Wiz"', '"Hair"', '"Rent"'],
-        image: "assets/images/"
+        image: "assets/images/wicked.gif"
     },
     {
         q: 'What musical features the song "If I Were a Rich Man"?',
         aIndex: 2,
         choices: ['"The Music Man"', '"My Fair Lady"', '"Fiddler on the Roof"', '"West Side Story"'],
-        image: "assets/images/"
+        image: "assets/images/fiddler.gif"
     },
     {
         q: 'What musical is based on the play "Pygmalion"?',
         aIndex: 2,
         choices: ['"The Music Man"', '"Chicago"', '"My Fair Lady"', '"The Sound of Music"'],
-        image: "assets/images/"
+        image: "assets/images/audrey.gif"
     },
     {
         q: 'Which character is described in song lyrics as the "Ten dollar founding father without a father"?',
-        aIndex: 1,
+        aIndex: 0,
         choices: ['"Alexander Hamilton"', '"George Washington"', '"Ben Franklin"', '"Sam Adams"'],
-        image: "assets/images/"
+        image: "assets/images/hamilton.gif"
     }
 ];
 
 var numQuestions;
-var questionsAsked;
+var numQuestionsAsked;
+var numCorrectAnswers;
+var numIncorrectAnswers;
+var numUnanswered;
+
 var timeLimit;
-var correctAnswers;
-var incorrectAnswers;
-var unanswered;
+var timeLeft;
+
+var questions;
+var intervalId;
+var currQuestion;
+var currAnswer;
+
 var isGameOver;
 
 var isQuestionUp;
 var isCorrect;
 
-var questions;
-var intervalId;
+
 
 // FUNCTIONS
 function startPage() {
+    clearPage();
+    $("#startBtn").show().click(startGame);
+}
+
+function clearPage() {
     $("#timeText, #qText, .choice, #resultsText, #detailsText").empty();
-    $("#startBtn").click(startGame);
+    $("img").hide();
+    $("#startBtn").hide();
 }
 
 function startGame() {
     numQuestions = 8;
-    questionsAsked = 0;
-    timeLimit = 10; //seconds
-    correctAnswers = 0;
-    incorrectAnswers = 0;
-    unanswered = 0;
-    isQuestionUp = false;
+    numQuestionsAsked = 0;
+    numCorrectAnswers = 0;
+    numIncorrectAnswers = 0;
+    numUnanswered = 0;
+    timeLimit = 5; //seconds
+    timeLeft = timeLimit;
     isGameOver = false;
+
+    isQuestionUp = false;
     isCorrect = false;
+
     $("#startBtn").hide();
     pickQuestions();
-    if (!isGameOver) {
-        nextQuestion();
-    } else {
-        displayEndPage();
-    }
+    nextQuestion();
 }
 
 function pickQuestions() {
@@ -144,19 +156,26 @@ function shuffle(a) {
 }
 
 function nextQuestion() {
-    displayQuestion();
-    startTimer();
-    // onChoiceClick();
-
+    if (numQuestions === numQuestionsAsked) isGameOver = true;
+    if (!isGameOver) {
+        displayQuestion();
+        startTimer();
+        onChoiceClick();
+    } else {
+        displayEndPage();
+    }
 }
 
 function displayQuestion() {
-    $("#timeText").html(`Time Remaining: <span id="countdown">${timeLimit}</span> seconds`);
-    $("#qText").text(questions[questionsAsked].q);
-    $("#0").text(questions[questionsAsked].choices[0]);
-    $("#1").text(questions[questionsAsked].choices[1]);
-    $("#2").text(questions[questionsAsked].choices[2]);
-    $("#3").text(questions[questionsAsked].choices[3]);
+    clearPage();
+    $("#timeText").html(`Time Remaining: <span id="countdown">${timeLeft}</span> seconds`);
+    currQuestion = questions[numQuestionsAsked].q
+    currAnswer = questions[numQuestionsAsked].choices[questions[numQuestionsAsked].aIndex];
+    $("#qText").text(currQuestion);
+    $("#0").text(questions[numQuestionsAsked].choices[0]);
+    $("#1").text(questions[numQuestionsAsked].choices[1]);
+    $("#2").text(questions[numQuestionsAsked].choices[2]);
+    $("#3").text(questions[numQuestionsAsked].choices[3]);
 }
 
 function startTimer() {
@@ -165,34 +184,43 @@ function startTimer() {
 }
 
 function decrement() {
-    console.log("started");
-    timeLimit--;
-    console.log(timeLimit);
-    $("#countdown").text(timeLimit);
-    if (timeLimit <= 0) {
-        clearInterval(intervalId);
+    timeLeft--;
+    $("#countdown").text(timeLeft);
+    if (timeLeft <= 0) {
+        stopTime();
         displayTimeoutPage();
-        timeLimit = 10;
+        setTimeout(nextQuestion, 5000);
     }
 }
 
-displayTimeoutPage() {
-    $("#timeText, #qText, .choice, #resultsText, #detailsText").empty();
-    $("#resultsText").text("All done, here's how you did!");
-    $("#detailsText").html(`<p>Correct Answers: ${correctAnswers}</p><p>Incorrect Answers: ${incorrectAnswers}</p><p>Unanswered: ${unanswered}</p>`);
+function stopTime() {
+    clearInterval(intervalId);
+    timeLeft = timeLimit;
+    numQuestionsAsked++;
+    console.log("numQuestionsAsked: " + numQuestionsAsked);
+}
+
+function displayTimeoutPage() {
+    clearPage();
+    $("#resultsText").text("Out of Time!");
+    $("#detailsText").html(`<p>The correct answer was: ${currAnswer}`);
+    $("img").attr("src", questions[numQuestionsAsked].image).attr("alt", currAnswer).show();
+    numUnanswered++;
+    console.log("numUnanswered: " + numUnanswered);
 }
 
 function onChoiceClick() {
-    questionsAsked++;
+    $(".choice").click(function() {
+        stopTime();
+        console.log(this.textContent === currAnswer);
+    });
 }
 
 function displayEndPage() {
-    $("#timeText, #qText, .choice, #resultsText, #detailsText").empty();
+    clearPage();
     $("#resultsText").text("All done, here's how you did!");
-    $("#detailsText").html(`<p>Correct Answers: ${correctAnswers}</p><p>Incorrect Answers: ${incorrectAnswers}</p><p>Unanswered: ${unanswered}</p>`);
+    $("#detailsText").html(`<p>Correct Answers: ${numCorrectAnswers}</p><p>Incorrect Answers: ${numIncorrectAnswers}</p><p>Unanswered: ${numUnanswered}</p>`);
 }
-
-
 
 // OBJECTS
 
